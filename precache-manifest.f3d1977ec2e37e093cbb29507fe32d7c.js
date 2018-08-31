38d828ffdd69f1fe2561431e4a5c219dd27195a0
Abubakari Sherifdeen <sherifdata@gmail.com>
2018-08-31 21:08:44 +0000

    ki

---
+++ precache-manifest.f3d1977ec2e37e093cbb29507fe32d7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "203ede0df3f42f5a2b54",
+    "url": "/js/chunk-vendors.def19c66.js"
+  },
+  {
+    "revision": "8cbeccdac97e8b03667f",
+    "url": "/js/app.8f5616bb.js"
+  },
+  {
+    "revision": "f8dc3a01ce57569509fd51ecc3ce0ae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f2574ee1d9bf806b9cba4586979b9d3b",
+    "url": "/img/loader.f2574ee1.gif"
+  },
+  {
+    "revision": "203ede0df3f42f5a2b54",
+    "url": "/css/chunk-vendors.cfbd759e.css"
+  },
+  {
+    "revision": "8cbeccdac97e8b03667f",
+    "url": "/css/app.7f09a449.css"
+  }
+];
